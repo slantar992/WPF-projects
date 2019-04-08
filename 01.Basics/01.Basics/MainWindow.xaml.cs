9f0ba549282744f935168cdaec1f2d9f50299739
Alberto Martin <albertouma92@gmail.com>
2019-04-08 17:38:34 +0200

    Add some functionality to "01.Basics" project

--- 01.Basics/01.Basics/MainWindow.xaml.cs
+++ 01.Basics/01.Basics/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace _01.Basics
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly string APPLIED = "Settings applied!";
+        private static readonly string APPLIED_STATUS = "{0}: Applied";
+        private static readonly string CLEAR_FIELDS_STATUS = "Fields clear!";
+        private static readonly string REFRESH_STATUS = "Refreshed!";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(APPLIED);
+            PrintStatus(string.Format(APPLIED_STATUS, DescriptionText.Text));
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetControls(FieldPanel.Children);
+            PrintStatus(CLEAR_FIELDS_STATUS);
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            PrintStatus(REFRESH_STATUS);
+        }
+
+        private void ResetControls(UIElementCollection elements)
+        {
+            foreach(object ctl in elements)
+            {
+                if (ctl.GetType().IsSubclassOf(typeof(Panel)))
+                    ResetControls(((Panel)ctl).Children);
+
+                if (ctl.GetType() == typeof(CheckBox))
+                    ((CheckBox)ctl).IsChecked = false;
+                if (ctl.GetType() == typeof(TextBox))
+                    ((TextBox)ctl).Text = "";
+            }
+        }
+
+        private void PrintStatus(string status)
+        {
+            StatusText.Text = status;
         }
     }
 }
